ba0d0b5dd942a05f4353a415d298799dd4ddb7c7
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-13 10:17:22 -0300

    Update Aluno.php

--- Aluno.php
+++ Aluno.php
@@ -1,1 +1,28 @@
+<?php
+
+class Aluno {
+    private $id;
+    private $nome;
+    private $turma;
+    
+
+    public function getId (){
+        return $this->id;
+    }
+    public function setId ($i){
+        $this->id = $i;
+    }
+    public function getNome (){
+        return $this->nome;
+    }
+    public function setNome ($i){
+        $this->nome = $i;
+    }
+    public function getTurma (){
+        return $this->turma;
+    }
+    public function setTurma ($i){
+        $this->turma = $i;
+    }
+}
 
